d6ea7cb43c7af12529972af76e024336c4f4dc50
Brendan Cronan <32400374+brendan-cronan@users.noreply.github.com>
2018-02-11 16:45:09 -0500

    Added docs to _Abs_GUI class, Created an outline for GameUtil class for common methods and created a class to generate a grid of lines for game board. (not finished)

--- GameHub/src/engine/_Abs_GUI.java
+++ GameHub/src/engine/_Abs_GUI.java
@@ -1,8 +1,22 @@
 package engine;
 
-public abstract class _Abs_GUI{
+public abstract class _Abs_GUI extends {
 
-  public void init();
+  /*
+  * This method will perform most of the setup for each GUI and will primarily
+  * call other methods and define instance variables.  Called from constructor.
+  */
+  public void init();//add parameters as needed.
+
+  /*
+  * This will return a Grid object that defines how many squares, rows/cols
+  * their width/height and any other aspects we need.
+  */
+  public Grid makeGrid();
+
+  /*
+  *
+  */
 
 
 
