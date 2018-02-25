9b1685d1391da6c0732e1b5b252bea20fef27cad
Brendan Cronan <32400374+brendan-cronan@users.noreply.github.com>
2018-02-25 17:38:18 -0500

    added a few files and a 'main' file

---
+++ GameHub/src/GameHub.java
@@ -0,0 +1,12 @@
+import gui;
+public class GameHub{
+
+
+  public static void main(String[] args){
+    Frame frame = new Frame("Game Hub", new Dimension(600,600));
+    frame.setVisible(true);
+
+
+  }
+
+}
